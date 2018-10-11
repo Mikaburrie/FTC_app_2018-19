a0fa83364fefcf7dc9e0453a6a07cd9f70e7d827
Mikaburrie <mikaburrie@gmail.com>
2018-10-10 21:06:06 -0400

    Make minor changes to class values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionProfiler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotionProfiler.java
@@ -5,14 +5,8 @@ package org.firstinspires.ftc.teamcode;
  */
 
 public class MotionProfiler {
-    public double maxSpeed;
-    public double acceleration;
-    private double accelerationTime;
-    private double accelerationDistance;
-    private double middleDistance;
-    private double middleTime;
-    private double totalTime;
-    private double t0;
+    private double maxSpeed;
+    private double acceleration;
     private double t1;
     private double t2;
     private double t3;
@@ -23,19 +17,18 @@ public class MotionProfiler {
     }
 
     public void setDistance(double distance) {
-        accelerationTime = maxSpeed / acceleration;
-        accelerationDistance = 1/2 * acceleration * Math.pow(accelerationTime, 2);
+        double accelerationTime = maxSpeed / acceleration;
+        double accelerationDistance = 1/2 * acceleration * Math.pow(accelerationTime, 2);
 
         if(accelerationDistance * 2 >= distance){
             accelerationDistance = 1/2 * distance;
             accelerationTime = Math.sqrt(2 * accelerationDistance / acceleration);
         }
 
-        middleDistance = distance - (2 * accelerationDistance);
-        middleTime = middleDistance / maxSpeed;
-        totalTime = middleTime + (2 * accelerationTime);
+        double middleDistance = distance - (2 * accelerationDistance);
+        double middleTime = middleDistance / maxSpeed;
+        double totalTime = middleTime + (2 * accelerationTime);
 
-        t0 = 0;
         t1 = accelerationTime;
         t2 = accelerationTime + middleTime;
         t3 = totalTime;
